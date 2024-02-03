a5281c239f63ab3adbd7e4d24c64231b03866188
Neel <neelnarayanshetty@gmail.com>
2024-02-04 00:31:56 +0530

    Updates

--- main.c
+++ main.c
@@ -1,10 +1,47 @@
-#include <errno.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define BF_FILE_LEN 8192
 #define MAX_FILE_NAME_LEN 256
+
+enum token_type {
+  MOVE_RIGHT = '>',
+  MOVE_LEFT = '<',
+  INCREMENT = '+',
+  DECREMENT = '-',
+  OUTPUT = '.',
+  INPUT = ',',
+  JUMP_FORWARD = '[',
+  JUMP_BACKWARD = ']',
+};
+
+struct token {
+  enum token_type type;
+  uint index;
+};
+
+struct instructions {
+  struct token *items;
+  size_t count;
+  size_t capacity;
+};
+
+struct instructions *init_instructions() {
+  struct instructions *instructions = malloc(sizeof(struct instructions));
+
+  instructions->items = NULL;
+  instructions->count = 0;
+  instructions->capacity = 0;
+
+  return instructions;
+}
+
+void free_instructions(struct instructions *instructions) {
+  free(instructions->items);
+  free(instructions);
+}
 
 char *read_bf_file(char *name) {
   FILE *f = NULL;
@@ -18,7 +55,7 @@ char *read_bf_file(char *name) {
     f = fopen(name, "r");
     if (NULL == f) {
       perror("failed to open file");
-      exit(1);
+      exit(EXIT_FAILURE);
     }
   }
 
@@ -41,9 +78,42 @@ char *read_bf_file(char *name) {
   return program;
 }
 
+void parse_instructions(struct instructions *instructions, char *program) {
+  for (size_t i = 0; i < strlen(program); i++) {
+    switch (program[i]) {
+    case '>':
+      break;
+    case '<':
+      break;
+    case '+':
+      break;
+    case '-':
+      break;
+    case '.':
+      break;
+    case ',':
+      break;
+    case '[':
+      break;
+    case ']':
+      break;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   char *program = NULL;
-  program = read_bf_file("hello-world.bfs");
-  printf("%s", program);
+  struct instructions *instructions = NULL;
+  // FILE *output;
+  // output = fopen("output.txt", "w+");
+  char *output;
+  program = read_bf_file("hello-world.bf");
+  // printf("%s", program);
+  instructions = init_instructions();
+  parse_instructions(instructions, program);
+
+  if (program != NULL) {
+    free(program);
+  }
   return 0;
 }
